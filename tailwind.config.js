7f19f278b57ac54b452d1415235ac3034b588ba7
shuyu0714 <upgedc13@hotmail.com>
2024-07-16 11:22:06 +0800

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
